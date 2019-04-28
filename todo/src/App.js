3f7c100400cdfdf6fb8319e239f0753dadf492b2
Aleksey Denisov <alde@pa3pa6o.tk>
2019-04-28 18:08:58 +0700

    выполнено задание по уроку "Что такое JSX"

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const val = 'я переменная';
+const logic = true;
+const tern = false;
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +17,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p style={{color: 'red'}}>Добавление тега style</p>
+        <p>Вывод значение переменной: {val}</p>
+        <p>Вывод числа: {34}</p>
+        <p>Вывод арифметической операции: {3 + 4}</p>
+        <p>Вывод результата логической операции: {logic && <span>Условие истинно))</span>}</p>
+        <p>Использование тернарного оператора: {tern ? <span>Условие истинно))</span> : <span>Все плохо(((</span>}</p>
+        <p>Вывод undefined: {undefined}, null:{null}, false:{false}, true:{true}</p>
       </div>
     );
   }
