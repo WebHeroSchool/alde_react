f4a561341e550a77128d12ebf7cdeb0f0b33d9d3
Aleksey Denisov <alde@pa3pa6o.tk>
2019-04-28 18:29:29 +0700

    выполнено задание по уроку "Модули"

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { count, lenght } from './number';
+
 const val = 'я переменная';
 const logic = true;
 const tern = false;
@@ -13,6 +15,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <h3>Общая длина: {count * lenght}</h3>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
---
+++ todo/src/number.js
@@ -0,0 +1,4 @@
+const count = 3;
+const lenght = 4;
+
+export { count, lenght };
