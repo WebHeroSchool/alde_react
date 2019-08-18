a3fd7f9db88a576c9dfdb841f574dc54093a33a3
Aleksey Denisov <alde.frontdev@gmail.com>
2019-08-18 16:56:35 +0700

    create class component

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
@@ -19,13 +19,17 @@ const itemList = [
   }
 ];
 
-const App = () => (
-    <div className={styles.wrap}>
-        <h1 className={styles.title}>Список задач</h1>
-        <InputItem />
-        <ItemList itemList={itemList}/>
-        <Footer count={3}/>
-    </div>
-);
+class App extends Component {
+  render () {
+    return (
+      <div className={styles.wrap}>
+          <h1 className={styles.title}>Список задач</h1>
+          <InputItem />
+          <ItemList itemList={itemList}/>
+          <Footer count={3}/>
+      </div>
+    )
+  }
+}
 
 export default App;
