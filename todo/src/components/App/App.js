2a63895faa4aba2c8ea0168c6dc83b3700711b4b
Aleksey Denisov <alde.frontdev@gmail.com>
2019-08-18 17:02:03 +0700

    create state

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -4,28 +4,31 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-const itemList = [
-  {
-    value: 'Поесть',
-    isDone: true,
-  },
-  {
-    value: 'Покодить',
-    isDone: true,
-  },
-  {
-    value: 'Поспать',
-    isDone: false,
-  }
-];
 
 class App extends Component {
+  state = {
+    itemList: [
+      {
+        value: 'Поесть',
+        isDone: true,
+      },
+      {
+        value: 'Покодить',
+        isDone: true,
+      },
+      {
+        value: 'Поспать',
+        isDone: false,
+      }
+    ],
+  };
+
   render () {
     return (
       <div className={styles.wrap}>
           <h1 className={styles.title}>Список задач</h1>
           <InputItem />
-          <ItemList itemList={itemList}/>
+          <ItemList itemList={this.state.itemList}/>
           <Footer count={3}/>
       </div>
     )
