14d4894d6fe4dd4a2551a2e55c28b11711877be6
Aleksey Denisov <alde.frontdev@gmail.com>
2019-08-21 09:48:17 +0700

    add checkboxHandler

--- todo/src/components/Item/Item.js
+++ todo/src/components/Item/Item.js
@@ -3,9 +3,13 @@ import classnames from 'classnames';
 import Clear from '@material-ui/icons/HighlightOff';
 import styles from './Item.module.css';
 
-const Item = ({task}) => (
+const Item = ({task, setTaskIsDone}) => (
     <div className={styles.itemWrap}>
-      <input type='checkbox'/>
+      <input
+          type='checkbox'
+          checked={task.isDone}
+          onClick={() => setTaskIsDone(task)}
+      />
       <div className={
         classnames({
           [styles.task]: task,
